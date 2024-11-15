e5acdfc101b0c94b7c2ecd08e4b318fa777e8679
Dhruv Gupta <121763005+Dhruv1Gupta@users.noreply.github.com>
2024-11-15 17:24:10 +0000

    Conversions of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "dhruv"
+
+// console.log(typeof score); //"dhruv"=>string //true=>boolean //undefined=>undefined //null=>object //"33"=>string //33=>number
+// console.log(typeof(score)); //"dhruv"=>string //true=>boolean //undefined=>undefined //null=>object //"33"=>string //33=>number
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //"dhruv"=>number //true=>number //undefined=>number //null=>number //"33abc"=>number //"33"=>number
+// console.log(valueInNumber); //"dhruv"=>NaN //true=>1 //undefined=>NaN //null=>0//"33abc"=>NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "dhruv"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //"dhruv"=>true //""=>false //1=>true
+
+//1=>true; 0=>false
+//""=>false
+//"dhruv"=>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber); // 33
+console.log(typeof stringNumber); // string
